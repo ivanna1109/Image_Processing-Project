17831f9d79e53604e2d53dc18ec0bb600b654644
Ivana <imilutinovic119@gmail.com>
2024-01-27 18:51:21 +0100

    sharpen fixed

--- Projekat/sharpen.py
+++ Projekat/sharpen.py
@@ -7,10 +7,10 @@ def sharpen_image(image):
         [0, -1, 0],
         [-1, 5, -1],
         [0, -1, 0]]) 
-    sharpened_image = np.zeros_like(image, dtype=np.float32)
-    for k in range(3): 
-        for i in range(1, height - 1):
-            for j in range(1, width - 1):
+    sharpened_image = np.zeros_like(image, dtype=np.float32) 
+    for i in range(1, height - 1):
+        for j in range(1, width - 1):
+            for k in range(3):
                 neighborhood = image[i - 1 : i + 2, j - 1 : j + 2, k] 
                 sharpened_image[i, j, k] = np.sum(neighborhood * matrix5x5)
     return np.clip(sharpened_image, 0, 255).astype(np.uint8)
