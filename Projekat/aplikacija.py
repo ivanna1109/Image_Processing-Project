78329534688245e92302b7b948ffb68481d348fb
Ivana <imilutinovic119@gmail.com>
2024-01-27 14:20:34 +0100

    slider motion added, but adjust is too slow

--- Projekat/aplikacija.py
+++ Projekat/aplikacija.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from PIL import Image, ImageTk
 import numpy as np
-#from histograms import histrogram_of_image
+from histogram import hist_to_image
 from adjust import rotate_image_bilinear
 from brightness import adjust_brightness
 from contrast import adjust_contrast_correctly
@@ -12,13 +12,14 @@ class Aplikacija:
     def __init__(self, root, o_image):
         self.prozor = root
         self.prozor.title("Uvod u procesiranje slike - Projekat")
-        self.prozor.geometry('920x650')
+        self.prozor.geometry('1380x650')
         self.left_frame = Frame(root, width=500, height=600, bg='grey')
         self.left_frame.grid(row=0, column=0, padx=10, pady=5)
         self.originalImage = o_image
         self.originalImageArray = np.array(o_image)
-        #histrogram_of_image(self.originalImageArray)
-        self.originalHistogram = Image.open('histograms/originalHistogram.png') #ovde histogram originalne slike i to se ne menja
+        self.originalRHistogram = hist_to_image(self.originalImageArray)
+        self.originalGHistogram = hist_to_image(self.originalImageArray)
+        self.originalBHistogram = hist_to_image(self.originalImageArray)
         self.filteredImage = None
         self.filteredHistogram  = None
         self.filteredImageArray = np.array(o_image)
@@ -28,54 +29,79 @@ class Aplikacija:
 
         Label(tool_bar, text="Filters", relief=RAISED).grid(row=0, column=0, padx=5, pady=3, ipadx=50)
 
+        #Adjust - Jako sporo radi
         self.adjustSlide = Scale(tool_bar, from_=-25, to=25, tickinterval=50, orient=HORIZONTAL, label='Adjust')
         self.adjustSlide.grid(row=2, column=0, padx=5, pady=5)
-        self.adjustSlide.bind("<ButtonRelease-1>", self.primeni_adjust)
+        self.clicked_adjust = False
+        self.adjustSlide.bind("<ButtonPress-1>", self.slider_adjust_pressed)
+        self.adjustSlide.bind("<ButtonRelease-1>", self.slider_adjust_released)
+        self.adjustSlide.bind("<Motion>", self.primeni_adjust)
 
+        #Brightness
         self.brightSlide = Scale(tool_bar, from_=-50, to=50, tickinterval=100, orient= HORIZONTAL, label='Brighteness')
         self.brightSlide.grid(row=3, column=0, padx=5, pady=5)
-        self.brightSlide.bind("<ButtonRelease-1>", self.primeni_brightness)
+        self.clicked_bright = False
+        self.brightSlide.bind("<ButtonPress-1>", self.slider_bright_pressed)
+        self.brightSlide.bind("<ButtonRelease-1>", self.slider_bright_released)
+        self.brightSlide.bind("<Motion>", self.primeni_brightness)
 
+        #Contrast
         self.contrastSlide = Scale(tool_bar, from_=0, to=100, tickinterval=50, orient=   HORIZONTAL, label='Contrast')
         self.contrastSlide.grid(row=4, column=0, padx=5, pady=5)
-        self.contrastSlide.bind("<ButtonRelease-1>", self.primeni_contrast)
+        self.clicked_contrast = False
+        self.contrastSlide.bind("<ButtonPress-1>", self.slider_contrast_pressed)
+        self.contrastSlide.bind("<ButtonRelease-1>", self.slider_contrast_released)
+        self.contrastSlide.bind("<Motion>", self.primeni_contrast)
 
+        #Warmth
         self.warmthSlide = Scale(tool_bar, from_=0, to=100, tickinterval=50, orient=   HORIZONTAL, label='Warmth')
         self.warmthSlide.grid(row=5, column=0, padx=5, pady=5)
         self.warmthSlide.bind("<ButtonRelease-1>", self.obradi_promenu_vrednosti)
 
+        #Saturation
         self.saturationSlide = Scale(tool_bar, from_=0, to=100, tickinterval=50, orient=   HORIZONTAL, label='Saturation')
         self.saturationSlide.grid(row=6, column=0, padx=5, pady=5)
         self.saturationSlide.bind("<ButtonRelease-1>", self.obradi_promenu_vrednosti)
 
+        #Fade
         self.fadeSlide = Scale(tool_bar, from_=-25, to=25, tickinterval=50, orient=   HORIZONTAL, label='Fade')
         self.fadeSlide.grid(row=7, column=0, padx=5, pady=5)
         self.fadeSlide.bind("<ButtonRelease-1>", self.obradi_promenu_vrednosti)
         
+        #Hightlights
         self.hightSlide = Scale(tool_bar, from_=0, to=100, tickinterval=50, orient=   HORIZONTAL, label='Hightlights')
         self.hightSlide.grid(row=2, column=1, padx=5, pady=5)
         self.hightSlide.bind("<ButtonRelease-1>", self.obradi_promenu_vrednosti)
 
+        #Shadows
         self.shadowSlide = Scale(tool_bar, from_=0, to=100, tickinterval=50, orient=   HORIZONTAL, label='Shadows')
         self.shadowSlide.grid(row=3, column=1, padx=5, pady=5)
         self.shadowSlide.bind("<ButtonRelease-1>", self.obradi_promenu_vrednosti)
 
+        #Zoom
         self.zoonSlide = Scale(tool_bar, from_=1, to=10, tickinterval=10, orient=HORIZONTAL, label='Zoom')
         self.zoonSlide.grid(row=4, column=1, padx=2, pady=2)
-        self.zoonSlide.bind("<ButtonRelease-1>", self.primeni_zoom)
+        self.clicked_zoom = False
+        self.zoonSlide.bind("<ButtonPress-1>", self.slider_zoom_pressed)
+        self.zoonSlide.bind("<ButtonRelease-1>", self.slider_zoom_released)
+        self.zoonSlide.bind("<Motion>", self.primeni_zoom)
 
+        #Vignette
         self.vignetteB = Button(tool_bar, text="Apply Vignette", width=6, height=1, borderwidth=5)
         self.vignetteB.grid(row=5, column=1, padx=2, pady=(0, 2), ipadx=20)
         self.vignetteB.bind("<Button-1>", self.obradi_promenu_vrednosti)
         
+        #Tile Shift - Linear
         self.tileShihtLB = Button(tool_bar, text="Apply Linear TS",  width=6, height=1, borderwidth=5)
         self.tileShihtLB.grid(row=6, column=1, padx=2, pady=2, ipadx=20)
         self.tileShihtLB.bind("<Button-1>", self.obradi_promenu_vrednosti)
 
+        #Tile Shift - Radial
         self.tileShihtRB = Button(tool_bar, text="Apply Radial TS",  width=6, height=1, borderwidth=5)
         self.tileShihtRB.grid(row=7, column=1, padx=2, pady=2, ipadx=20)
         self.tileShihtRB.bind("<Button-1>", self.obradi_promenu_vrednosti)
 
+        #Sharpen
         self.sharpenB = Button(tool_bar, text="Apply Sharpen", width=6, height=1, borderwidth=5)
         self.sharpenB.grid(row=8, column=1, padx=2, pady=(2,0), ipadx=20)
         self.sharpenB.bind("<Button-1>", self.obradi_promenu_vrednosti)
@@ -83,51 +109,80 @@ class Aplikacija:
         self.right_frame = Frame(root, width=650, height=400, bg='black')
         self.right_frame.grid(row=0, column=2, padx=10, pady=5)
         self.image = ImageTk.PhotoImage(o_image)
-        self.histImage = ImageTk.PhotoImage(self.originalHistogram)
+        self.histRImage = ImageTk.PhotoImage(self.originalRHistogram)
+        self.histGImage = ImageTk.PhotoImage(self.originalGHistogram)
+        self.histBImage = ImageTk.PhotoImage(self.originalBHistogram)
         #Right frame
         Label(self.right_frame, image=self.image).grid(row=0,column=0, padx=5, pady=5)
-        Label(self.right_frame, image=self.histImage).grid(row=0,column=2, padx=5, pady=5)
+        Label(self.right_frame, image=self.histRImage).grid(row=0,column=2, padx=5, pady=5)
+        Label(self.right_frame, image=self.histGImage).grid(row=0,column=4, padx=5, pady=5)
+        Label(self.right_frame, image=self.histBImage).grid(row=0,column=6, padx=5, pady=5)
         """
         Label(self.right_frame, image=self.filteredImage).grid(row=2,column=0, padx=5, pady=5)
         Label(self.right_frame, image=self.filteredHistogram).grid(row=2,column=2, padx=5, pady=5)"""
 
     def obradi_promenu_vrednosti(self, event):
-        print(self.adjustSlide.get())
-        tmp = rotate_image_bilinear(self.imageArray, self.adjustSlide.get())
-        print(type(tmp))
-        newImage = ImageTk.PhotoImage(image=Image.fromarray(tmp))
-        self.image = newImage
-        self.imageArray = tmp
-        Label(self.right_frame, image=self.image).grid(row=0,column=0, padx=5, pady=5)
+        pass
+
+    #Adjust
+    def slider_adjust_pressed(self, event):
+        self.clicked_adjust = True       
+            
+    def slider_adjust_released(self, event):
+        self.clicked_adjust = False
 
     def primeni_adjust(self, event):
-        tmp = rotate_image_bilinear(self.originalImageArray, self.adjustSlide.get())
-        self.image = ImageTk.PhotoImage(image=Image.fromarray(tmp))
-        self.filteredImageArray = tmp
-        print("Adjust done.")
-        Label(self.right_frame, image=self.image).grid(row=0,column=0, padx=5, pady=5)
-        
+        if self.clicked_adjust:
+            tmp = rotate_image_bilinear(self.originalImageArray, self.adjustSlide.get())
+            self.image = ImageTk.PhotoImage(image=Image.fromarray(tmp))
+            self.filteredImageArray = tmp
+            print("Adjust done.")
+            Label(self.right_frame, image=self.image).grid(row=0,column=0, padx=5, pady=5)
+
+    #Bright
+    def slider_bright_pressed(self, event):
+        self.clicked_bright = True       
+            
+    def slider_bright_released(self, event):
+        self.clicked_bright = False   
+
     def primeni_brightness(self, event):
-        tmp = adjust_brightness(self.filteredImageArray, self.brightSlide.get())
-        self.image = ImageTk.PhotoImage(image=Image.fromarray(tmp))
-        self.imageArray = tmp
-        print("Brightness done.")
-        Label(self.right_frame, image=self.image).grid(row=0,column=0, padx=5, pady=5)
+        if self.clicked_bright:
+            tmp = adjust_brightness(self.filteredImageArray, self.brightSlide.get())
+            self.image = ImageTk.PhotoImage(image=Image.fromarray(tmp))
+            self.imageArray = tmp
+            print("Brightness done.")
+            Label(self.right_frame, image=self.image).grid(row=0,column=0, padx=5, pady=5)
 
+    #Contrast
+    def slider_contrast_pressed(self, event):
+        self.clicked_contrast = True       
+            
+    def slider_contrast_released(self, event):
+        self.clicked_contrast = False   
+    
     def primeni_contrast(self, event):
-        tmp = adjust_contrast_correctly(self.filteredImageArray, self.contrastSlide.get())
-        self.image = ImageTk.PhotoImage(image=Image.fromarray(tmp))
-        self.imageArray = tmp
-        print("Contrast done.")
-        Label(self.right_frame, image=self.image).grid(row=0,column=0, padx=5, pady=5)
+        if self.clicked_contrast:
+            tmp = adjust_contrast_correctly(self.filteredImageArray, self.contrastSlide.get())
+            self.image = ImageTk.PhotoImage(image=Image.fromarray(tmp))
+            self.imageArray = tmp
+            print("Contrast done.")
+            Label(self.right_frame, image=self.image).grid(row=0,column=0, padx=5, pady=5)
 
+    #Zoom
+    def slider_zoom_pressed(self, event):
+        self.clicked_zoom = True       
+            
+    def slider_zoom_released(self, event):
+        self.clicked_zoom = False
+    
     def primeni_zoom(self, event):
-        print(self.zoonSlide.get())
-        tmp = zoom_image(self.originalImage, self.zoonSlide.get())
-        self.image = ImageTk.PhotoImage(image=Image.fromarray(tmp))
-        self.imageArray = tmp
-        print("Zoom done.")
-        Label(self.right_frame, image=self.image).grid(row=0,column=0, padx=5, pady=5)
+        if self.clicked_zoom:
+            tmp = zoom_image(self.originalImage, self.zoonSlide.get())
+            self.image = ImageTk.PhotoImage(image=Image.fromarray(tmp))
+            self.imageArray = tmp
+            print("Zoom done.")
+            Label(self.right_frame, image=self.image).grid(row=0,column=0, padx=5, pady=5)
         
 
 
--- Projekat/histogram.py
+++ Projekat/histogram.py
@@ -1,4 +1,5 @@
 import numpy as np
+from PIL import Image, ImageDraw
 
 def hist(img):
     hist_img = np.zeros(256)
@@ -8,14 +9,29 @@ def hist(img):
     
     return hist_img
 
+def hist_to_image(img_array):
+    histogram = hist(img_array)
+    max_count = np.max(histogram)
+    height = img_array.shape[0]  
+    width = len(histogram) 
+    histogram_image = Image.new("RGB", (width, height), "white")
+
+    draw = ImageDraw.Draw(histogram_image)
+    for i, count in enumerate(histogram):
+        bar_height = int(count / max_count * height)
+        draw.line((i, height, i, height - bar_height), fill="blue")
+
+    return histogram_image
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
-    path = r'D:\_PMF\_IntroductionToImageProcessing\Project\Image_Processing-Project\Image_Processing-Project\ProjekatIIP\src\images\building.jpg' 
-    img = plt.imread(path)
+    #path = r'D:\_PMF\_IntroductionToImageProcessing\Project\Image_Processing-Project\Image_Processing-Project\ProjekatIIP\src\images\building.jpg' 
+    img = plt.imread('images/test.JPG')
     img = np.array(img)
-    hist_img = hist(img)
-
+    hist_array = hist(img)
+    hist_image = hist_to_image(hist_array)
+    """
     plt.bar(range(256), hist_img)
     plt.show()
+    """
